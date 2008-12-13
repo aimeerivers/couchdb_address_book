db2e28f1892deff1819a8ea0e3a5b188ee38e291
sermoa <aimee@mychores.co.uk>
2008-12-13 17:00:41 +0000

    Set up the layout options for the cheapo labels from the pound shop!

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,10 +1,26 @@
 class ContactsController < ApplicationController
 
+    require 'pdf/writer'
+    include PDF
+    
   def index
     @contacts = Contact.view(database_name, 'contacts/by_last_name-map')
     respond_to do |format|
       format.pdf do
-        pdf = PdfLabelMaker.avery_labels(@contacts.rows)
+        options = {
+          :columns => 3,
+          :labels_per_page => 30,
+          :left_margin => Writer.mm2pts(4),
+          :bottom_margin => Writer.mm2pts(25),
+          :label_width => Writer.mm2pts(67),
+          :label_height => Writer.mm2pts(26.3),
+          :label_padding_x => Writer.mm2pts(4),
+          :label_padding_y => Writer.mm2pts(3),
+          :gap_between_labels_x => Writer.mm2pts(4),
+          :font_size =>  9,
+          :line_height =>  10
+        }
+        pdf = PdfLabelMaker.avery_labels(@contacts.rows, 'user-defined', options)
         send_data pdf.render, :filename => 'labels.pdf', :type => 'application/pdf'
       end
       format.html
